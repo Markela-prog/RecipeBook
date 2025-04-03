1d8978c225762e9c42e99032980ead52830b8597
unknown <rxtmark@gmail.com>
2025-04-03 14:53:20 +0300

    Modules optimization

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [ShoppingListComponent, ShoppingEditComponent],
+  imports: [
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'shopping-list',
+        component: ShoppingListComponent,
+      },
+    ]),
+  ],
+  exports: [],
+})
+export class ShoppingListModule {}
